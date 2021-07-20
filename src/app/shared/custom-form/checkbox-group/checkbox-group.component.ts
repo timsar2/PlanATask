3261d68df0082ad4493cc48735a2c1af3470a7d4
tetra <info.golab@gmail.com>
2021-07-20 21:48:07 +0300

    Implement change detection strategy for control value accessor

--- src/app/shared/custom-form/checkbox-group/checkbox-group.component.ts
+++ src/app/shared/custom-form/checkbox-group/checkbox-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
@@ -11,32 +11,40 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
       useExisting: forwardRef(() => CheckboxGroupComponent),
       multi: true
     }
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CheckboxGroupComponent implements ControlValueAccessor {
-  private onChange: (m: any) => void;
+  // private onChange: (m: any) => void;
   private onTouched: (m: any) => void;
   _model: any;
   items: boolean[];
 
-  constructor() { }
+  constructor(private ref: ChangeDetectorRef) { }
 
   toggleCheck(index: number, event: Event) {
     this._model[index] = (event.target as HTMLInputElement).checked;
   }
-
+  onChange(m: any){
+  }
   get model() {
     return this._model;
   }
 
   writeValue(value: any): void {
     this._model = value;
-    if(value != null){
-      this.items = [];
-      this._model.forEach(element => {
-        this.items.push(element);
-      });
-    }
+    
+    // detect change manualy because we want to change object index inside of ngfor
+    this.ref.detectChanges();
+    this.ref.detach();
+
+    //* Instead of using new obj to prevent missing index on ngfor, now we use change detaction strategy*/
+    // if(value != null){
+    //   this.items = [];
+    //   this._model.forEach(element => {
+    //     this.items.push(element);
+    //   });
+    // }
   }
 
   registerOnChange(fn: any): void {
